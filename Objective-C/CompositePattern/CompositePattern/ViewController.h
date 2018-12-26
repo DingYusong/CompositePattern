ee3153c2c0684292e36f4f72e791d93b155c17e8
丁玉松 <yusong.ding@huangjinqianbao.com>
2018-12-26 10:54:29 +0800

    first commit

---
+++ Objective-C/CompositePattern/CompositePattern/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CompositePattern
+//
+//  Created by 丁玉松 on 2018/12/26.
+//  Copyright © 2018 丁玉松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Objective-C/CompositePattern/CompositePattern/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CompositePattern
+//
+//  Created by 丁玉松 on 2018/12/26.
+//  Copyright © 2018 丁玉松. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
